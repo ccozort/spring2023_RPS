d85e37fcf1e2418f6ecd210098694ee69dc54509
Chris Cozort <chriscozort@gmail.com>
2023-03-03 08:46:40 -0800

    First push

---
+++ rps.py
@@ -0,0 +1,146 @@
+# File created by: Chris Cozort
+# 
+ 
+# import libraries
+
+from time import sleep
+
+from random import randint 
+
+import pygame as pg
+
+import os
+
+game_folder = os.path.dirname(__file__)
+print(game_folder)
+
+# game settings
+WIDTH = 800
+HEIGHT = 600
+FPS = 30
+
+# define colors
+# tuples are immutable - cannot change once created
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+choices = ["rock", "paper", "scissors"]
+
+def draw_text(text, size, color, x, y):
+    font_name = pg.font.match_font('arial')
+    font = pg.font.Font(font_name, size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x,y)
+    screen.blit(text_surface, text_rect)
+
+def cpu_randchoice():
+    choice = choices[randint(0,2)]
+    print("computer randomly decides..." + choice)
+    return choice
+
+pg.init()
+pg.mixer.init()
+
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Rock, Paper, Scissors...")
+clock = pg.time.Clock()
+
+rock_image = pg.image.load(os.path.join(game_folder, 'rock.jpg')).convert()
+rock_image_rect = rock_image.get_rect()
+cpu_rock_image_rect = rock_image.get_rect()
+
+paper_image = pg.image.load(os.path.join(game_folder, 'paper.jpg')).convert()
+paper_image_rect = paper_image.get_rect()
+
+scissors_image = pg.image.load(os.path.join(game_folder, 'scissors.png')).convert()
+scissors_image_rect = scissors_image.get_rect()
+
+you_image = pg.image.load(os.path.join(game_folder, 'scissors.png')).convert()
+scissors_image_rect = scissors_image.get_rect()
+
+start_screen = True
+
+player_choice = ""
+cpu_choice = ""
+running = True
+
+while running:
+    clock.tick(FPS)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        # ########## input ###########
+        # HCI - human computer interaction...
+        # keyboard, mouse, controller, vr headset
+        
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                print("game on!!!!")
+                start_screen = False
+        
+        if event.type == pg.MOUSEBUTTONUP:
+            # 
+            print(pg.mouse.get_pos()[0])
+            # 
+            print(pg.mouse.get_pos()[1])
+            # 
+            mouse_coords = pg.mouse.get_pos()
+
+            print(rock_image_rect.collidepoint(mouse_coords))
+            if rock_image_rect.collidepoint(mouse_coords):
+                print("you clicked on rock..")
+                player_choice = "rock"
+                cpu_choice = cpu_randchoice()
+                # call a function that gets the cpu choice...
+            elif paper_image_rect.collidepoint(mouse_coords):
+                print("you clicked on paper...")
+                player_choice = "paper"
+                cpu_choice = cpu_randchoice()
+       
+            elif scissors_image_rect.collidepoint(mouse_coords):
+                print("you clicked on scissors...")
+                player_choice = "scissors"
+                cpu_choice = cpu_randchoice()                      
+            else:
+                print("you didn't click on anything...")
+    
+    
+    
+
+    ############ draw ###################
+    screen.fill(BLACK)
+
+    # waits for player to hit space bar
+    if start_screen:
+        draw_text("Press space to play rock paper scissors", 22, WHITE, WIDTH/2, HEIGHT/10)
+        rock_image_rect.x = 2000
+        paper_image_rect.x = 2000
+        scissors_image_rect.x = 2000
+
+    # allows player to choose rock paper or scissors
+    if not start_screen and player_choice == "":
+        rock_image_rect.x = 50
+        paper_image_rect.x = 350
+        scissors_image_rect.x = 550
+        screen.blit(scissors_image, scissors_image_rect)
+        screen.blit(paper_image, paper_image_rect)
+        screen.blit(rock_image, rock_image_rect)
+
+    # 
+    if player_choice == "rock":
+        if cpu_choice == "rock":
+            cpu_rock_image_rect.x = 500
+            screen.blit(rock_image, rock_image_rect)
+            screen.blit(rock_image, cpu_rock_image_rect)
+            draw_text("You tied!!!", 22, WHITE, WIDTH/2, HEIGHT/10)
+
+
+
+    pg.display.flip()
+
+pg.quit()
